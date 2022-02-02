c1e1081c011f23f8a02375a70695a0e67515a621
Mattia de' Michieli Vitturi <demichie@gmail.com>
2022-02-02 20:44:31 +0100

    Add files via upload

---
+++ createWebform.py
@@ -0,0 +1,137 @@
+import pandas as pd
+import streamlit as st
+import os.path
+
+# HACK This only works when we've installed streamlit with pipenv, so the
+# permissions during install are the same as the running process
+STREAMLIT_STATIC_PATH = pathlib.Path(st.__path__[0]) / 'static'
+# We create a downloads directory within the streamlit static asset directory
+# and we write output files to it
+DOWNLOADS_PATH = (STREAMLIT_STATIC_PATH / "downloads")
+if not DOWNLOADS_PATH.is_dir():
+    DOWNLOADS_PATH.mkdir()
+
+def check_form(qst,ans,units):
+
+    n_qst = int((len(qst)-2)/3)
+    
+    check_flag = True
+
+    for i in range(n_qst):
+    
+        idx = 2+i*3
+    
+        if not ans[idx].isnumeric():
+        
+            st.write('Non numeric answer')
+            st.write(qst[idx],ans[idx])
+            check_flag = False
+            
+        if not ans[idx+1].isnumeric():
+        
+            st.write('Non numeric answer')
+            st.write(qst[idx+1],ans[idx+1])
+            check_flag = False
+            
+        if not ans[idx+2].isnumeric():
+        
+            st.write('Non numeric answer')
+            st.write(qst[idx+2],ans[idx+2])
+            check_flag = False
+            
+        if check_flag:    
+            
+            if float(ans[idx]) >= float(ans[idx+1]):
+        
+                st.write('Error. '+qst[idx]+' >= '+qst[idx+1])            
+                check_flag = False
+            
+            if float(ans[idx+1]) >= float(ans[idx+2]):
+        
+                st.write('Error. '+qst[idx+1]+' >= '+qst[idx+2])            
+                check_flag = False
+            
+            if units[i] == '%':
+            
+                if float(ans[idx])<= 0.0 or float(ans[idx])>= 100.0:
+                
+                    st.write('Error. '+qst[idx])
+                    st.write('The answer must be a percentage value (0<x<100)')
+                    check_flag = False
+            
+                if float(ans[idx+1])<= 0.0 or float(ans[idx+1])>= 100.0:
+                
+                    st.write('Error. '+qst[idx+1])
+                    st.write('The answer must be a percentage value (0<x<100)')
+                    check_flag = False
+            
+                if float(ans[idx+2])<= 0.0 or float(ans[idx+2])>= 100.0:
+                
+                    st.write('Error. '+qst[idx+2])
+                    st.write('The answer must be a percentage value (0<x<100)')
+                    check_flag = False
+
+    return check_flag       
+
+def main():
+
+    st.title("Elicitation form")
+	
+    csv_file = 'test_input.csv'
+    df = pd.read_csv(csv_file,header=0)
+    
+    output_file = csv_file.replace('.csv','_NEW.csv')
+
+    pctls = [5,50,95]
+
+    form2 = st.form(key='form2')
+    
+    qst = ["First Name"]
+    ans = []
+    
+    ans.append(form2.text_input(qst[-1]))
+    
+    qst.append("Last Name")
+    ans.append(form2.text_input(qst[-1]))
+    
+    units = []
+    for i in df.itertuples():
+        idx,shortQ,longQ,unit,scale = i
+        units.append(unit)
+        # print(idx,qst,unit,scale)
+        form2.header(shortQ)
+        form2.markdown(longQ)
+        j=0
+        for pct in pctls:
+            j+=1
+            
+            qst.append(shortQ+' - '+str(int(pct))+'% ['+unit+']')
+    
+            ans.append(form2.text_input(qst[-1]))
+            
+    submit_button2 = form2.form_submit_button("Submit")
+    
+    if not os.path.exists(output_file):
+    
+        df_new = pd.DataFrame(columns=qst) 
+        df_new.to_csv(output_file)  
+        
+    if submit_button2:
+    
+        check_flag = check_form(qst,ans,units)
+        
+        if check_flag:
+    
+            st.write('Thank you '+ans[0]+' '+ans[1] )
+        
+            zip_iterator = zip(qst,ans)
+            data = dict(zip_iterator)
+            
+        
+            df_old = pd.read_csv(output_file,header=0, index_col=[0])
+            df_new = df_old.append(data, ignore_index = True)
+            
+            open(output_file, 'w').write(df_new.to_csv())
+        
+if __name__ == '__main__':
+	main()            
