50c196af95b93d28b6c8bfdf53414981350a0061
Mattia de' Michieli Vitturi <demichie@gmail.com>
2022-04-21 20:59:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,7 +123,7 @@ def main():
     for i in df.itertuples():
     
         idx,shortQ,longQ,unit,scale,minVal,maxVal,realization,question = i[0:9]
-	print(idx,shortQ,longQ,unit,scale,minVal,maxVal,realization,question)
+        print(idx,shortQ,longQ,unit,scale,minVal,maxVal,realization,question)
         
         if ( question == quest_type):
 
