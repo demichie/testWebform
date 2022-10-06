cf3f494c7ddc27c186b1b83e1276be87ea260c86
Mattia de' Michieli Vitturi <demichie@gmail.com>
2022-10-06 17:01:30 +0200

    Create createWebformDict.py

---
+++ createWebformDict.py
@@ -0,0 +1,15 @@
+input_dir = 'DATA' 
+csv_file = 'questionnaire.csv'
+
+# this can be 'seed' or 'target'
+quest_type = 'target'
+
+idx_list = [1,2,3,4,5]
+
+# select 'github' or 'local'
+datarepo = 'github'
+
+# user = 'username'
+# github_token = "token"
+
+pctls = [5, 50, 95]
