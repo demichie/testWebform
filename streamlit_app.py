177c985b70d8e57f173f0a757b17506bc157902f
Mattia de' Michieli Vitturi <demichie@gmail.com>
2022-10-06 14:53:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ csv_file = 'questionnaire.csv'
 # this can be 'seed' or 'target'
 quest_type = 'target'
 
-idx_list = [1]
+idx_list = [1,2,3,4,5]
 
 # select 'github' or 'local'
 datarepo = 'github'
