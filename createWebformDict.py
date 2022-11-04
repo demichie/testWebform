8b4e6ead5c59222becea3e569daaf0b5b7951b52
Mattia de' Michieli Vitturi <demichie@gmail.com>
2022-11-04 11:06:07 +0100

    Update createWebformDict.py

--- createWebformDict.py
+++ createWebformDict.py
@@ -5,10 +5,12 @@ csv_file = 'questionnaire.csv'
 # this can be 'seed' or 'target'
 quest_type = 'target'
 
-idx_list = [1,2,3,4,5]
+target_list = [2,3]
+seed_list = [1,2]
+absolute_indexing = True
 
 # select 'github' or 'local'
-datarepo = 'github'
+datarepo = 'local'
 
 # user = 'username'
 # github_token = "token"
