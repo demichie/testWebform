2ed4b510f94f68c6b31d9353c28594873c4f627e
Mattia de' Michieli Vitturi <demichie@gmail.com>
2022-11-16 22:11:30 +0100

    upd

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,8 @@ from createWebformDict import *
 
 import smtplib
 from email import encoders
+from email.mime.base import MIMEBase
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.header import Header
